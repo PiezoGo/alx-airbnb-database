40358f6a8fab08aeba777bf743acf2654435474f
PiezoGo <piezo5go@gmail.com>
2025-07-09 06:50:20 +0300

    Creating indexes

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,13 +1,16 @@
-SELECT 
-    user_id,
-    COUNT(*) AS total_bookings
-FROM 
-    Booking
-GROUP BY 
-    user_id;
+CREATE INDEX Inddex1 
+on Property (property_id);
 
+CREATE INDEX Index2
+on Booking (property_id);
 
-SELECT property_id, total_bookings, 
+CREATE INDEX Index3
+on User (user_id);
+
+CREATE INDEX Index4
+on User (created_at);
+
+EXPLAIN ANALYZE SELECT property_id, total_bookings, 
 RANK() OVER(ORDER BY total_bookings DESC) AS booking_cts
 FROM 
 (SELECT Booking.property_id,
@@ -16,3 +19,4 @@ FROM `Booking`
 JOIN `Property`
 ON `Booking`.property_id = `Property`.property_id
 GROUP BY `Property`.property_id) AS booking_counts ;
+
